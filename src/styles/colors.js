bcddc9890d636bdcb2afceaa8de1d1d1175d56e4
Felipe Soares <felipeluizsoares@gmail.com>
2017-04-15 01:57:33 -0300

    Add colors variables

---
+++ src/styles/colors.js
@@ -0,0 +1,7 @@
+export const primaryColor = '#f79b2e'
+export const secondaryColor = '#49638e'
+export const primaryLightColor = '#ffa55b'
+export const secondaryLightColor = '#74a7ff'
+export const tertiaryColor = '#f25454'
+export const halfOpacityColor = 'rgba(0,0,0,.5)'
+export const textPrimaryColor = '#fff'
